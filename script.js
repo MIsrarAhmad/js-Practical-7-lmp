150f0d5cdc2878e92e0305e5adc7a96e0ee8f649
MIsrarAhmad <m.israrahmad2021@gmail.com>
2024-07-16 21:15:38 -0700

    JS Practical 7 Imp* done

---
+++ script.js
@@ -0,0 +1,29 @@
+// let str = prompt("Enter a string")
+// let strInd = prompt("Enter n index number to check");
+// strInd = str.charAt(str);
+// alert(strInd)
+
+
+// Task2
+// let str = prompt("Enter a string");
+// let strInd = prompt("Enter substring to check");
+// let strInd2 = str.includes(strInd);
+// let strInd3 = str.replace(strInd,"M.IsrarAhmad");
+// alert(strInd3);
+
+// Task3
+// let str = +prompt("Enter Number: ");
+// let str2 = Math.round(str)
+// alert(str2)
+
+// Task4
+// let str = +prompt("Enter Number: ");
+// let str2 = Math.ceil(str)
+// alert(str2)
+
+
+// Task5
+// let str = +prompt("Enter Number: ");
+// let str2 = Math.floor(str)
+// alert(str2)
+
